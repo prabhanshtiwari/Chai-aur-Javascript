9b04bda9315496658a6e4b346b3dc63646fe9b9c
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2025-03-29 15:59:54 +0530

    variables and constants in js

---
+++ Code/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2; // not allowed
+
+accountEmail = "hc@hc.com";
+accountPassword = "554664";
+accountCity = "Bengluru";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
