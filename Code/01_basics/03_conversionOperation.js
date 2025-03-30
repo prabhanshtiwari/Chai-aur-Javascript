28f323e11130c8225281737e2074a18023d449f2
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2025-03-30 13:30:39 +0530

    conversion

---
+++ Code/01_basics/03_conversionOperation.js
@@ -0,0 +1,121 @@
+/*
+let score = 33;
+
+console.log(typeof score); // number
+console.log(typeof(score)); // number
+*/
+/*
+let score = "33";
+
+console.log(typeof score); // string
+console.log(typeof(score)); // string
+*/
+/*
+let score = "33";
+
+console.log(typeof score); // string
+console.log(typeof(score)); // string
+
+let valueInNumber = Number(score); // convert string to number
+console.log(typeof valueInNumber); // number
+*/
+/*
+let score = "33abc";
+
+console.log(typeof score); // string
+console.log(typeof(score)); // string
+
+let valueInNumber = Number(score); // convert string to number
+console.log(valueInNumber); // NaN
+console.log(typeof valueInNumber); // number
+*/
+/*
+let score = null;
+
+console.log(typeof score); // object
+console.log(typeof(score)); // object
+
+let valueInNumber = Number(score); // convert string to number
+console.log(valueInNumber); // 0
+console.log(typeof valueInNumber); // number
+*/
+
+/*
+let score = undefined;
+
+console.log(typeof score); // undefined
+console.log(typeof(score)); // undefined
+
+let valueInNumber = Number(score); // convert string to number
+console.log(valueInNumber); // Nan
+console.log(typeof valueInNumber); // number
+*/
+
+/*
+let score = true;
+
+console.log(typeof score); // boolean
+console.log(typeof(score)); // boolean
+
+let valueInNumber = Number(score); // convert string to number
+console.log(valueInNumber); // 1
+console.log(typeof valueInNumber); // number
+*/
+
+/*
+let score = "hitesh";
+
+console.log(typeof score); // boolean
+console.log(typeof(score)); // boolean
+
+let valueInNumber = Number(score); // convert string to number
+console.log(valueInNumber); // NaN
+console.log(typeof valueInNumber); // number
+*/
+
+/*
+"33" => 33
+"33abc" => NaN
+rue => 1
+false => 0
+null => 0
+*/
+/*
+let isLoggedIn = 1;
+
+let booleanILoggedIn = Boolean(isLoggedIn);
+console.log(booleanILoggedIn); // true
+*/
+/*
+
+let isLoggedIn = "";
+
+let booleanILoggedIn = Boolean(isLoggedIn);
+console.log(booleanILoggedIn); // false
+*/
+/*
+let isLoggedIn = "hitesh";
+
+let booleanILoggedIn = Boolean(isLoggedIn);
+console.log(booleanILoggedIn); // true
+*/
+
+/*
+1 => true
+0 => false
+"" => false
+"abc" => true
+null => false
+undefined => false
+NaN => false
+false => false
+true => true
+*/
+/*
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber); // "33"
+console.log(typeof stringNumber); // string
+*/
+
