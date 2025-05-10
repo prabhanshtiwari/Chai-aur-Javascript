df6de0256d037268158545ae75b0c405eef3694c
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2025-05-11 00:34:54 +0530

    lecture 18  completed

--- Code/02_basics/04_objects.js
+++ Code/02_basics/04_objects.js
@@ -18,7 +18,7 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname);
+// console.log(regularUser.fullname.userfullname);
 
 
 // optional chaining
@@ -59,18 +59,55 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
 // important
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
 
-console.log(Object.entries(tinderUser)); 
+// console.log(Object.entries(tinderUser)); 
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
-
-
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
 
 
 
 
+// De-structuring is done for Arrays and Objects both
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh",
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// in react - destructuring
+// const navbar = ({company}) => {
+
+// } 
+// navbar(company = "hitesh")
+
+
+// API intro
+
+// json 
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// Sometimes APIs can be in Array format also
+
+[
+    {},
+    {},
+    {}
+]
