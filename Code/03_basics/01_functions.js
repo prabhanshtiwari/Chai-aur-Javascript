fc6aa099faa0554a1cc9798ac7c57aa3662eb4ec
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2025-05-11 16:29:50 +0530

    lecture 20 done

--- Code/03_basics/01_functions.js
+++ Code/03_basics/01_functions.js
@@ -51,9 +51,45 @@ function loginUserMessage(userName = "sam") {
 // console.log(loginUserMessage("Hitesh"));
 
 // common in interviews
-console.log(loginUserMessage()); // undefined
+// console.log(loginUserMessage()); // undefined
 
 
 // default values of parameters 
 
 
+
+// rest/spread operator
+// function calculateCartPrice(...num1) {
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+// rest vs spread operator
+
+// derive concept from the below code
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1;
+}
+// console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: "Hitesh",
+    price: 199,
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user);
+// handleObject({ username: "sam", price: 399 });
+
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+    return getArray[1];
+}
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200, 400, 100, 600]));
