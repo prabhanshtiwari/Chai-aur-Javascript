64f6c9c3c9aff84289339811a64d0d9aeed91eae
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2025-05-12 06:30:02 +0530

    lecture 22 completed

--- Code/03_basics/02_scopes.js
+++ Code/03_basics/02_scopes.js
@@ -20,7 +20,7 @@
 
 // console.log(a); // a is not defined
 // console.log(b); // b is not defined
-console.log(c); // 30
+// console.log(c); // 30
 
 // Why var is avoided by most of the people?
 
@@ -32,16 +32,65 @@ if (true) {
     let a = 10;
     const b = 20;
     var c = 30;
-    console.log("INNER ", a);
+    // console.log("INNER ", a);
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
-console.log(a); 
+// console.log(a); 
 // console.log(b); 
 // console.log(c);
+
+
+
+
+// Nested Scope
+
+function one() {
+    const username = "hitesh";
+
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website); 
+
+    two();
+}
+
+// one();
+
+
+if (true) {
+    const username = "hitesh";
+    if (username === "hitesh") {
+        const website = " youtube";
+        // console.log(username + website);
+    }
+    // console.log(website); // error
+}
+
+// console.log(username); /// error
+
+
+
+// ++++++++intresting+++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1;
+}
+
+
+
+addTwo(5); // error
+// function expression
+const addTwo = function(num){
+    return num + 2;
+}
 
 
