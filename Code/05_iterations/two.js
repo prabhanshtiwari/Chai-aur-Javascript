46e1d6631030ba0a742be65dcfef9a4dcc44f846
Prabhansh Tiwari <prabhanshtiwari2002@gmail.com>
2025-05-14 17:52:26 +0530

    while & do-while completed

---
+++ Code/05_iterations/two.js
@@ -0,0 +1,31 @@
+
+// while loop
+// while loop is used when you want to execute the code until a certain condition is met
+
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+// do while loop
+// do while loop is used when you want to execute the code at least once
+// even if the condition is false
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
